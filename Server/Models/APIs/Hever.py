2e6cbaa18fac11331088daef1e0e6bd12ef7f514
Bar M <bar.maor.1997@gmail.com>
2018-08-25 18:16:46 +0200

    Maybe its working idk

--- Server/Models/APIs/Hever.py
+++ Server/Models/APIs/Hever.py
@@ -2,6 +2,7 @@
 from Models.Response import Response
 from flask import Blueprint
 import requests
+import json
 
 BLUEPRINT = Blueprint(__name__, '', url_prefix='/Hever')
 
@@ -29,7 +30,7 @@ class Hever(object):
         return x not in content.decode('cp1255')
 
     @staticmethod
-    def login(user_name, password):
+    def login(user_name, password, session, cookies):
         data = {'tz': user_name,
                 'password': password,
                 'oMode': 'login',
@@ -41,21 +42,33 @@ class Hever(object):
                    'Origin': 'https://www.hvr.co.il',
                    'Upgrade-Insecure-Requests': '1',
                    'Connection': 'keep-alive',
-                   'Cookie': 'bn=800262434,552569166741807636; ASP.NET_SessionId=5wt0goq5nazngz2o4ua35cuh; zoom=1',
+                   'Cookie': cookies,
                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'}
 
-        with requests.Session() as session:
-            response = session.post(Hever.HEVER_LOGIN_URL, data=data, headers=headers, verify=True)
-            if Hever.is_logged_in(response.content):
-                print 'Connected to hever'
-                headers = {'Cookie': 'zoom=1; ASP.NET_SessionId=fstf5zuychovulr2hl52xvu5; init_code=9142309255; tid=4230925; home_page=hvr_home; bn=; email=Bar.maor.1997@gmail.com; userfullname=%u05D1%u05E8%20%u05DE%u05D0%u05D5%u05E8; logout=signin.aspx' }
-                response = session.get(Hever.HEVER_HOME_PAGE, headers=headers)
-                text = response.text.encode('latin1').decode('cp1255')
-                Hever.write_file(text.encode('cp1255'))
-            else:
-                print 'Couldnt connect'
+        print headers
+        response = session.post(Hever.HEVER_LOGIN_URL, data=data, headers=headers, verify=True)
+        print response.content
+        print response.text
+        print response.status_code
+        '''
+        if Hever.is_logged_in(response.content):
+            print 'Connected to hever'
+            headers = {'Cookie': 'zoom=1; ASP.NET_SessionId=fstf5zuychovulr2hl52xvu5; init_code=9142309255; tid=4230925; home_page=hvr_home; bn=; email=Bar.maor.1997@gmail.com; userfullname=%u05D1%u05E8%20%u05DE%u05D0%u05D5%u05E8; logout=signin.aspx' }
+            response = session.get(Hever.HEVER_HOME_PAGE, headers=headers)
+            text = response.text.encode('latin1').decode('cp1255')
+            Hever.write_file(text.encode('cp1255'))
+        else:
+            print 'Couldnt connect'
+        '''
 
+    @staticmethod
+    def get_cookies(session):
+        session.get(Hever.HEVER_LOGIN_URL)
+        return session.cookies.get_dict()
 
     @staticmethod
     def test():
-        Hever.login('', '')
+        session = requests.Session()
+        cookies = json.dumps(Hever.get_cookies(session)).lstrip('{').rstrip('}').replace('"', '')
+        print cookies
+        Hever.login('', '', session, cookies)
